81ba9e60fe881cf0aba6837092f5717765cbeb0c
Fábio Balbé <fabiobalbe@mac.local>
2026-02-09 11:57:08 -0300

    Auto-format in save para jinja

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,13 +1,26 @@
 local M = {}
 
-M.formatters_by_ft = {
-  lua = { "stylua" },
-  python = { "isort", "black" },
+M.options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = { "isort", "black" },
+
+    -- Jinja / Django-style HTML templates
+    htmldjango = { "djlint" },
+  },
+
+  formatters = {
+    djlint = {
+      command = "djlint",
+      args = { "--reformat", "-" },
+      stdin = true,
+    },
+  },
+
+  format_on_save = {
+    timeout_ms = 2000,
+    lsp_fallback = false,
+  },
 }
 
-M.format_on_save = {
-  timeout_ms = 2000,
-  lsp_fallback = false,
-}
-
-return M
+return M.options
